fe7d9c2d8739f6531d879b3fcdfd2b2d66baa0e7
Prateek Chandan <prateekchandan@google.com>
2025-09-06 13:21:54 +0000

    we have a custom background now with an overlay, the overlay is dark, wh

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -4,7 +4,7 @@ export function Background() {
       className="fixed inset-0 z-[-1] bg-contain"
       style={{ backgroundImage: "url('/treasure-map-background.png')" }}
     >
-      <div className="absolute inset-0 bg-black/60 backdrop-blur-[2px]" />
+      <div className="absolute inset-0 bg-white/70 dark:bg-black/70 backdrop-blur-[2px]" />
     </div>
   );
 }
